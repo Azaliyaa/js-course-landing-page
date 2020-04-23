6bfe46bbbd51c5409f9781d577a7dfa2a0565997
Azaliia Kasimova <kasimova@reksoft.ru>
2020-04-23 20:37:30 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -90,4 +90,25 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   setClock('timer', deadline);
+
+  // Modal
+
+  let knowMore = document.querySelectorAll('.more, .description-btn'),
+      more = document.querySelector('.more'),
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.popup-close');
+
+  for (let more of knowMore) {
+    more.addEventListener('click', function() {
+      overlay.style.display = 'block';
+      this.classList.add('more-splash');
+      document.body.style.overflow = 'hidden';
+    });
+  }
+
+  close.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  });
 });
